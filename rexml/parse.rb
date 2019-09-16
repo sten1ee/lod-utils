1628241439b9ff8c5e63eac17a82e6c81eb50fa8
Stanislav Jordanov <stenlee@gmail.com>
2019-09-16 22:21:25 +0300

    refactored to different parse_xxx and report_xxx methods

--- rexml/parse.rb
+++ rexml/parse.rb
@@ -6,27 +6,34 @@ require 'rexml/document'
 class TeiParser
   include REXML
 
-  GLOSSARY = STDOUT
+  OUT = STDOUT
 
   def initialize(filter:nil)
     @filter = filter
-    @type_count = Hash.new(0)
-    @id_count = Hash.new(0)
   end
 
-  def process_dir(dir = '.')
+  def process_dir(dir:, entity:)
+    parse_method = method("parse_" + entity.to_s)
+    report_method = method("report_" + entity.to_s)
+
     Dir.glob('*.xml', base: dir) do |tei_file|
-      process_file "#{dir}/#{tei_file}"
+      parse_method.call("#{dir}/#{tei_file}")
     end
-    self
+
+    report_method.call
   end
 
-  def process_file(tei_file)
+  def parse_gloss(tei_file)
     doc = Document.new File.new tei_file
     doc.elements.each('//gloss') do |elm|
       text = elm.to_s
-      GLOSSARY << "<!-- #{tei_file} -->\n" << text << "\n\n" if @filter === text
+      if @filter === text
+        OUT << "<!-- #{tei_file} -->\n" << text << "\n\n"
+      end
 
+      @type_count ||= Hash.new(0)
+      @id_count ||= Hash.new(0)
+  
       type = elm.attributes['type']
       @type_count[type] += 1
 
@@ -34,14 +41,54 @@ class TeiParser
       cnt = @id_count[id] += 1
       raise(ArgumentError, "Duplicate xml:id: #{id} !") if cnt != 1
     end
-    self
   end
 
-  def write_reports(out)
+  def report_gloss
     @type_count = @type_count.sort_by { |_type, count| -count }.to_h
-    out << @type_count << "\n"
+    OUT << @type_count << "\n"
   end
-  self
+
+  def parse_ref(tei_file)
+    doc = Document.new File.new tei_file
+    doc.elements.each('//ref') do |elm|
+      text = elm.to_s
+      if @filter === text
+        OUT << "<!-- #{tei_file} -->\n" << text << "\n\n"
+      end
+
+      @ref_attr ||= Hash.new { |h, k| h[k] = Set.new }
+      elm.attributes.each { |attr, val| @ref_attr[attr] << val }
+
+      if (cref = elm.attributes['cRef'])
+        @cref_map ||= Hash.new { |h, k| h[k] = Array.new }
+        @cref_map[cref] << tei_file
+      end
+    end
+  end
+
+  def report_ref
+    <<~COMMENT
+    @cref_map
+      .filter { |cref, occurances| occurances.size > 1 }
+      .sort_by { |cref, occurances| cref }
+      .each do |cref, occurances|
+        OUT << cref << " =>\n"
+        occurances.each do |tei_file|
+          OUT << "\t" << tei_file.to_s.split('/').last << "\n"
+        end
+        OUT << "\n"
+      end
+    COMMENT
+
+    @ref_attr.each do |attr, values|
+      next if attr == 'cRef' || attr == 'key'
+      OUT << attr << " =>\n"
+      values.each do |value|
+        OUT << "\t" << value << "\n"
+      end
+      OUT << "\n"
+    end
+  end
 end
 
 # doc = XmlSimple.xml_in('061-013_toh193,739_prophecy_of_shri_mahadevi.xml')
@@ -50,13 +97,15 @@ if __FILE__ == $PROGRAM_NAME
   puts "ruby #{RUBY_VERSION}p#{RUBY_PATCHLEVEL} running"
   puts "command line: #{$PROGRAM_NAME} #{ARGV.join}"
   raise ArgumentError, 'Too many command line args!' if ARGV.size > 1
-  raise ArgumentError, 'Missing command line arg for report filter! Supply "*" to match all.' if ARGV.size < 1
-  filter = ARGV[0]
-  filter = '.*' if filter.empty? || filter == '*'
-  #raise ArgumentError, "Not a .csv file name specified - '#{ARGV[0]}'" if not ARGV[0].end_with? '.csv'
+  if ARGV.size < 1 || ARGV[0].empty?
+    STDERR << 'Missing command line arg for report filter! Assuming match all.'
+    filter = '.*'
+  else
+    filter = ARGV[0]
+    filter = '.*' if (filter.empty? || filter == '*')
+  end
 
-  parser = TeiParser.new(filter: Regexp.new(filter, Regexp::IGNORECASE)) #/[Bb]hagav[āa][nt]/  #/[Pp][āa][ṇn][ḍd]ava/
-               .process_dir('translations')
-               .write_reports STDOUT
-  parser
+  parser = TeiParser
+             .new(filter: Regexp.new(filter, Regexp::IGNORECASE)) #/[Bb]hagav[āa][nt]/  #/[Pp][āa][ṇn][ḍd]ava/
+             .process_dir(dir: 'translations', entity: :ref)
 end
